db70e3ca1839df6a7a1e650460d0fc34c9b13ece
euthiagodealmeida <github@thiagodealmeida.me>
2025-06-10 21:44:54 -0300

    chore (s3-example): change example files to reflect s3 module

--- aws/examples/s3/main.tf
+++ aws/examples/s3/main.tf
@@ -15,22 +15,12 @@ provider "aws" {
 module "lab-s3" {
   source = "../../s3"
 
-  bucket_name                = var.bucket_name
-  force_destroy             = var.force_destroy
-  versioning_status         = var.versioning_status
-  sse_algorithm             = var.sse_algorithm
-  kms_key_id               = var.kms_key_id
-  bucket_key_enabled       = var.bucket_key_enabled
-  block_public_acls        = var.block_public_acls
-  block_public_policy      = var.block_public_policy
-  ignore_public_acls       = var.ignore_public_acls
-  restrict_public_buckets  = var.restrict_public_buckets
-  bucket_policy_count      = var.bucket_policy_count
-  public_read_policy       = var.public_read_policy
-  website_config_count     = var.website_config_count
-  website_index_document   = var.website_index_document
-  website_error_document   = var.website_error_document
-  website_endpoint         = var.website_endpoint
-  website_domain          = var.website_domain
-  tags                    = var.tags
+  bucket_name        = var.bucket_name
+  force_destroy      = var.force_destroy
+  enable_versioning  = var.enable_versioning
+  kms_key_id         = var.kms_key_id
+  bucket_key_enabled = var.bucket_key_enabled
+  enable_public_read = var.enable_public_read
+  website_config     = var.website_config
+  tags               = var.tags
 }
--- aws/examples/s3/outputs.tf
+++ aws/examples/s3/outputs.tf
@@ -18,32 +18,54 @@ output "bucket_regional_domain_name" {
   value       = module.lab-s3.bucket_regional_domain_name
 }
 
+output "bucket_hosted_zone_id" {
+  description = "The Route 53 Hosted Zone ID for this bucket's region"
+  value       = module.lab-s3.bucket_hosted_zone_id
+}
+
 output "bucket_region" {
   description = "The AWS region this bucket resides in"
   value       = module.lab-s3.bucket_region
 }
 
-output "bucket_versioning_status" {
-  description = "The versioning state of the bucket"
-  value       = module.lab-s3.bucket_versioning_status
+output "bucket_website_endpoint" {
+  description = "The website endpoint, if the bucket is configured with a website"
+  value       = module.lab-s3.bucket_website_endpoint
 }
 
-output "bucket_encryption_algorithm" {
-  description = "The encryption algorithm used for the bucket"
-  value       = module.lab-s3.bucket_encryption_algorithm
+output "bucket_website_domain" {
+  description = "The domain of the website endpoint, if the bucket is configured with a website"
+  value       = module.lab-s3.bucket_website_domain
 }
 
-output "bucket_kms_key_id" {
-  description = "The KMS key ID used for bucket encryption"
-  value       = module.lab-s3.bucket_kms_key_id
+output "bucket_versioning_enabled" {
+  description = "Whether versioning is enabled on the bucket"
+  value       = module.lab-s3.bucket_versioning_enabled
 }
 
-output "bucket_key_enabled" {
-  description = "Whether S3 Bucket Key is enabled for SSE-KMS"
-  value       = module.lab-s3.bucket_key_enabled
+output "bucket_public_read_enabled" {
+  description = "Whether public read access is enabled"
+  value       = module.lab-s3.bucket_public_read_enabled
 }
 
-output "bucket_tags" {
-  description = "All tags applied to the bucket"
-  value       = module.lab-s3.bucket_tags
+output "bucket_website_enabled" {
+  description = "Whether website configuration is enabled"
+  value       = module.lab-s3.bucket_website_enabled
+}
+
+output "bucket_kms_encrypted" {
+  description = "Whether the bucket uses KMS encryption"
+  value       = module.lab-s3.bucket_kms_encrypted
+}
+
+output "bucket_summary" {
+  description = "Summary of the created S3 bucket"
+  value = {
+    name              = var.bucket_name
+    region            = var.aws_region
+    versioning        = var.enable_versioning
+    public_read       = var.enable_public_read
+    website_enabled   = var.website_config != null
+    kms_encrypted     = var.kms_key_id != null
+  }
 }
